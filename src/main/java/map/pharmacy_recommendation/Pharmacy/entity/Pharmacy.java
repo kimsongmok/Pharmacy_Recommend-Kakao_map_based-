aab775b69a3220e4b55631ab0675c29b6d516689
mok0618@naver.com <mok0618@naver.com>
2024-02-11 22:13:48 +0900

    test

--- src/main/java/map/pharmacy_recommendation/Pharmacy/entity/Pharmacy.java
+++ src/main/java/map/pharmacy_recommendation/Pharmacy/entity/Pharmacy.java
@@ -19,7 +19,6 @@ public class Pharmacy {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private String  pharmacyName;
   private String  pharmacyAddress;
   private double  latitude;
