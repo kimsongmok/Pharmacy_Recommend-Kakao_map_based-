d86972fdd370b4e85a3bea1c6f4fdb0cd6bf91d3
mok0618@naver.com <mok0618@naver.com>
2024-02-11 21:42:35 +0900

    수정

--- src/main/java/map/pharmacy_recommendation/api/service/KakaoUriBuilderService.java
+++ src/main/java/map/pharmacy_recommendation/api/service/KakaoUriBuilderService.java
@@ -13,11 +13,13 @@ public class KakaoUriBuilderService {
   private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
 
   public URI buildUriByAddressSearch(String address){
+
     UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
     uriBuilder.queryParam("query", address);
     URI uri = uriBuilder.build().encode().toUri();
     log.info("[KakaoUriBuilderService buildUriByAddressSearch] address: {}, uri: {}", address, uri);
 
     return uri;
+
   }
 }
